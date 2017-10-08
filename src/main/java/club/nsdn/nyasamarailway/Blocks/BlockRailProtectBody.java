6d807a6478c5989e66f4292c8b17522bc1f3e1ef
drzzm32 <drzzm32@gmail.com>
2017-10-09 00:24:34 +0800

    fix rail

--- src/main/java/club/nsdn/nyasamarailway/Blocks/BlockRailProtectBody.java
+++ src/main/java/club/nsdn/nyasamarailway/Blocks/BlockRailProtectBody.java
@@ -35,14 +35,14 @@ public class BlockRailProtectBody extends BlockRailBase implements IRailSpeedKee
 
     @Override
     public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack itemStack) {
-        int meta = MathHelper.floor_double(((double)(player.rotationYaw * 4.0F / 360.0F) + 0.5D) * 2) & 7;
+        int meta = MathHelper.floor_double(((double)(player.rotationYaw * 4.0F / 360.0F) * 2) + 0.5D) & 7;
 
         if (meta == 0 || meta == 4) meta = 0;
         else if (meta == 2 || meta == 6) meta = 1;
-        else if (meta == 1) meta = 6;
-        else if (meta == 3) meta = 9;
-        else if (meta == 5) meta = 8;
-        else meta = 7;
+        else if (meta == 1) meta = 9;
+        else if (meta == 3) meta = 6;
+        else if (meta == 5) meta = 7;
+        else meta = 8;
 
         world.setBlockMetadataWithNotify(x, y, z, meta, 2);
     }
