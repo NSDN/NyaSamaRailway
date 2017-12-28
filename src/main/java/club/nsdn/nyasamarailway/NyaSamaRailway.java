a397c27bde3a6e4252f1ed3cd8925a72cf842703
drzzm32 <drzzm32@gmail.com>
2017-12-28 22:55:12 +0800

    modify version

--- src/main/java/club/nsdn/nyasamarailway/NyaSamaRailway.java
+++ src/main/java/club/nsdn/nyasamarailway/NyaSamaRailway.java
@@ -17,7 +17,7 @@ public class NyaSamaRailway {
     @Mod.Instance("NyaSamaRailway")
     public static NyaSamaRailway instance;
     public static final String MODID = "NyaSamaRailway";
-    public static final String VERSION = "0.5";
+    public static final String VERSION = "1.0";
     public static final boolean isDebug = false;
     public static Logger log = LogManager.getLogger(MODID);
 
