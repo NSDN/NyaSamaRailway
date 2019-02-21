925afe8836843458faf89c6f10c2249b0df76807
drzzm32 <drzzm32@gmail.com>
2019-02-21 23:47:26 +0800

    fix import

--- src/main/java/club/nsdn/nyasamarailway/util/SoundUtil.java
+++ src/main/java/club/nsdn/nyasamarailway/util/SoundUtil.java
@@ -9,7 +9,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 import java.util.LinkedList;
 
