02925bcb71274f7f65eea72af87e6a80f3204ffb
drzzm32 <drzzm32@gmail.com>
2020-03-31 17:08:03 +0800

    network fix

--- src/main/java/club/nsdn/nyasamarailway/network/PacketCtSHandler.java
+++ src/main/java/club/nsdn/nyasamarailway/network/PacketCtSHandler.java
@@ -2,11 +2,11 @@ package club.nsdn.nyasamarailway.network;
 
 import club.nsdn.nyasamarailway.item.tool.ItemNTP32Bit;
 import club.nsdn.nyasamarailway.item.tool.ItemNTP8Bit;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.item.ItemStack;
 
 /**
  * Created by drzzm32 on 2019.2.10
@@ -16,22 +16,24 @@ public class PacketCtSHandler implements IMessageHandler<TrainPacket, IMessage> 
     public IMessage onMessage(TrainPacket packet, MessageContext context) {
         EntityPlayerMP serverPlayer = context.getServerHandler().player;
 
-        ItemStack stack = serverPlayer.getHeldItemMainhand();
-        if (!stack.isEmpty()) {
-            if (stack.getItem() instanceof ItemNTP8Bit) {
-                ItemNTP8Bit ntp8Bit = (ItemNTP8Bit) stack.getItem();
-                ntp8Bit.power.set(stack, packet.P);
-                ntp8Bit.brake.set(stack, packet.R);
-                ntp8Bit.dir.set(stack, packet.Dir);
-                ntp8Bit.mode.set(stack, packet.Mode);
-                ntp8Bit.mblk.set(stack, packet.MBlk);
-            } else if (stack.getItem() instanceof ItemNTP32Bit) {
-                ItemNTP32Bit ntp32Bit = (ItemNTP32Bit) stack.getItem();
-                ntp32Bit.power.set(stack, packet.P);
-                ntp32Bit.brake.set(stack, packet.R);
-                ntp32Bit.dir.set(stack, packet.Dir);
+        serverPlayer.getServerWorld().addScheduledTask(() -> {
+            ItemStack stack = serverPlayer.getHeldItemMainhand();
+            if (!stack.isEmpty()) {
+                if (stack.getItem() instanceof ItemNTP8Bit) {
+                    ItemNTP8Bit ntp8Bit = (ItemNTP8Bit) stack.getItem();
+                    ntp8Bit.power.set(stack, packet.P);
+                    ntp8Bit.brake.set(stack, packet.R);
+                    ntp8Bit.dir.set(stack, packet.Dir);
+                    ntp8Bit.mode.set(stack, packet.Mode);
+                    ntp8Bit.mblk.set(stack, packet.MBlk);
+                } else if (stack.getItem() instanceof ItemNTP32Bit) {
+                    ItemNTP32Bit ntp32Bit = (ItemNTP32Bit) stack.getItem();
+                    ntp32Bit.power.set(stack, packet.P);
+                    ntp32Bit.brake.set(stack, packet.R);
+                    ntp32Bit.dir.set(stack, packet.Dir);
+                }
             }
-        }
+        });
 
         return null;
     }
--- src/main/java/club/nsdn/nyasamarailway/network/PacketStCHandler.java
+++ src/main/java/club/nsdn/nyasamarailway/network/PacketStCHandler.java
@@ -2,11 +2,12 @@ package club.nsdn.nyasamarailway.network;
 
 import club.nsdn.nyasamarailway.item.tool.ItemNTP32Bit;
 import club.nsdn.nyasamarailway.item.tool.ItemNTP8Bit;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 
 /**
  * Created by drzzm32 on 2019.2.10
@@ -19,22 +20,24 @@ public class PacketStCHandler implements IMessageHandler<TrainPacket, IMessage> 
     public IMessage onMessage(TrainPacket packet, MessageContext context) {
         if (player == null) return null;
 
-        ItemStack stack = player.getHeldItemMainhand();
-        if (!stack.isEmpty()) {
-            if (stack.getItem() instanceof ItemNTP8Bit) {
-                ItemNTP8Bit ntp8Bit = (ItemNTP8Bit) stack.getItem();
-                ntp8Bit.power.set(stack, packet.P);
-                ntp8Bit.brake.set(stack, packet.R);
-                ntp8Bit.dir.set(stack, packet.Dir);
-                ntp8Bit.mode.set(stack, packet.Mode);
-                ntp8Bit.mblk.set(stack, packet.MBlk);
-            } else if (stack.getItem() instanceof ItemNTP32Bit) {
-                ItemNTP32Bit ntp32Bit = (ItemNTP32Bit) stack.getItem();
-                ntp32Bit.power.set(stack, packet.P);
-                ntp32Bit.brake.set(stack, packet.R);
-                ntp32Bit.dir.set(stack, packet.Dir);
+        Minecraft.getMinecraft().addScheduledTask(() -> {
+            ItemStack stack = player.getHeldItemMainhand();
+            if (!stack.isEmpty()) {
+                if (stack.getItem() instanceof ItemNTP8Bit) {
+                    ItemNTP8Bit ntp8Bit = (ItemNTP8Bit) stack.getItem();
+                    ntp8Bit.power.set(stack, packet.P);
+                    ntp8Bit.brake.set(stack, packet.R);
+                    ntp8Bit.dir.set(stack, packet.Dir);
+                    ntp8Bit.mode.set(stack, packet.Mode);
+                    ntp8Bit.mblk.set(stack, packet.MBlk);
+                } else if (stack.getItem() instanceof ItemNTP32Bit) {
+                    ItemNTP32Bit ntp32Bit = (ItemNTP32Bit) stack.getItem();
+                    ntp32Bit.power.set(stack, packet.P);
+                    ntp32Bit.brake.set(stack, packet.R);
+                    ntp32Bit.dir.set(stack, packet.Dir);
+                }
             }
-        }
+        });
 
         return null;
     }
